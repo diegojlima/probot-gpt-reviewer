5cd6438894bc3d59574c58b5a3b07010e0699d53
dj-delima <diego.delima@acrolinx.com>
2023-05-24 00:03:41 +0200

    test: get issue event payload

--- src/events/handle-comment-created.ts
+++ src/events/handle-comment-created.ts
@@ -5,21 +5,12 @@ export default async (context: Context): Promise<void> => {
     console.log('GOT INTO ISSUE EVENT')
     const octokit = context.octokit;
 
+    console.log(JSON.stringify(context.payload, null, 2));
+
     // Check if the bot is mentioned in the comment and if the comment is on a pull request
     // @ts-ignore
     const commentBody = context.payload.comment.body;
     const isBotMentioned = commentBody.includes('@probot-gpt-reviewer');
-
-    // @ts-ignore
-    const pullRequest = context.payload.pull_request;
-    const title = pullRequest.title;
-    const description = pullRequest.body;
-    const messages = [
-        { role: 'system', content: 'You are a helpful AI that reviews code and being questioned about previous comments' },
-        { role: 'user', content: `Pull Request Title: ${title}` },
-        { role: 'user', content: `Pull Request Description: ${description}` },
-    ];
-
     // @ts-ignore
     const isPullRequest = 'pull_request' in context.payload.issue;
 
@@ -27,6 +18,22 @@ export default async (context: Context): Promise<void> => {
         const repo = context.repo();
         // @ts-ignore
         const pullRequestNumber = context.payload.issue.number;
+
+        // Get the pull request details
+        const pullRequestResponse = await octokit.pulls.get({
+            ...repo,
+            pull_number: pullRequestNumber,
+        });
+
+        const pullRequest = pullRequestResponse.data;
+        const title = pullRequest.title;
+        const description = pullRequest.body;
+
+        const messages = [
+            { role: 'system', content: 'You are a helpful AI that reviews code and being questioned about previous comments' },
+            { role: 'user', content: `Pull Request Title: ${title}` },
+            { role: 'user', content: `Pull Request Description: ${description}` },
+        ];
 
         // Fetch all the comments on the pull request
         const comments = await octokit.issues.listComments({
@@ -44,7 +51,6 @@ export default async (context: Context): Promise<void> => {
         const question = commentBody.split('@probot-gpt-reviewer')[1].trim(); // get the text after the bot's name
         messages.push({ role: 'user', content: `Question: ${question}` });
         console.log(question);
-
 
         const gptResponse = await sendToGpt(messages);
 
